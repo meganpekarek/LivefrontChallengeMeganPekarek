3150c083f1a7b58f0502e0a20d7c9593f173fe52
Megan Pekarek <mpekarek9@gmail.com>
2021-01-27 16:52:02 -0600

    using react testing library to test page with async call

--- src/viewsTests/RecipeDetails.test.js
+++ src/viewsTests/RecipeDetails.test.js
@@ -2,30 +2,44 @@ import React from 'react';
 import Adapter from 'enzyme-adapter-react-16';
 import {shallow, configure} from 'enzyme';
 import RecipeDetails from '../views/RecipeDetails';
+import { act, render, cleanup, waitFor } from '@testing-library/react'
+import axios from 'axios'
 
-configure({adapter: new Adapter()});
+
+const recipe = {
+    meals: [
+        {
+            idMeal: '52772',
+            strMeal: 'testMeal',
+            strIngredient1: 'Chicken',
+            strMeasure1: '1',
+            strInstructions: 'Instructions',
+            strMealThumb: ''
+        }
+    ]
+}
 
 const routeComponentPropsMock = {
     history: {},
     location: {
         state: {
             recipe: {
-                idMeal: '12345'
+                idMeal: '52772'
             }
         }
     },
     match: {}
 }
+beforeEach(() => {
+  axios.get = jest.fn(() => Promise.resolve({ data: recipe }))
+})
+afterEach(cleanup)
 
 describe('RecipeDetails', () => {
-  it('displays proper elements when loading state is true', () => {
-
-    const recipeDetails = shallow(
-        <RecipeDetails
-            {...routeComponentPropsMock}
-        />
-    );
-
-    expect(recipeDetails.find('RecipeDetailsLoader')).toHaveLength(1);
-  });
-});
+    it('displays the recipe details', async () => {
+        await act(async () => {
+            const { getByText } = render(<RecipeDetails {...routeComponentPropsMock} />)
+            await waitFor(() => getByText('testMeal'))
+        })
+    })
+})
